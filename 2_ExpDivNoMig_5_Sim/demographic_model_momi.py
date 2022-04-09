f6516495ac5c368618d22368fa8e062de376a02f
Kate Noskova <ekaterina.e.noskova@gmail.com>
2022-04-09 22:09:44 +0300

    add momi model

--- 2_ExpDivNoMig_5_Sim/demographic_model_momi.py
+++ 2_ExpDivNoMig_5_Sim/demographic_model_momi.py
@@ -8,6 +8,6 @@ def model_func(params):
     model.add_leaf("pop0", N=nu1_gen*Nanc, g=0)
     model.add_leaf("pop1", N=nu2_gen*Nanc, g=0)
 
-    model.move_lineages("pop0", "pop1", t=T2_gen*2*Nanc, N=Nanc, g=np.log(nu_gen/1.0) / (T1_gen*2*Nanc))
-    nodel.set_size("pop0", N=nu_gen*Nanc, g=0, t=(T1_gen+T2_gen)*2*Nanc)
+    model.move_lineages("pop0", "pop1", t=T2_gen*2*Nanc, N=nu_gen*Nanc, g=np.log(nu_gen/1.0) / (T1_gen*2*Nanc))
+    nodel.set_size("pop0", N=Nanc, g=0, t=(T1_gen+T2_gen)*2*Nanc)
     return model
---
+++ 2_YRI_CEU_6_Gut/demographic_model_momi.py
@@ -0,0 +1,13 @@
+import momi
+import numpy as np
+
+def model_func(params):
+    Nanc, nu1F_gen, nu2B_gen, nu2F_gen, Tp_gen, T_gen = params
+
+    model = momi.DemographicModel(N_e=1)
+    model.add_leaf("pop0", N=nu1F_gen*Nanc, g=0)
+    model.add_leaf("pop1", N=nu2F_gen*Nanc, g=np.log(nu_gen/nu2B_gen) / (T_gen*2*Nanc))
+
+    model.move_lineages("pop0", "pop1", t=T_gen*2*Nanc, N=nu1F_gen*Nanc, g=0)
+    nodel.set_size("pop0", N=Nanc, g=0, t=(Tp_gen+T_gen)*2*Nanc)
+    return model
